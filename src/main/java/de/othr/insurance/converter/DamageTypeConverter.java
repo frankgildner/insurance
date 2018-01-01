03eb3f0976b99e92f07b60fc89f44d0be3f025e2
Frank <test@test.com>
2018-01-01 16:50:22 +0100

    damageTypeConverter

--- src/main/java/de/othr/insurance/converter/DamageTypeConverter.java
+++ src/main/java/de/othr/insurance/converter/DamageTypeConverter.java
@@ -1,5 +1,6 @@
 package de.othr.insurance.converter;
 
+import de.othr.insurance.entity.DamageType;
 import de.othr.insurance.service.DamageCaseService;
 import de.othr.insurance.service.DamageTypeService;
 import javax.enterprise.context.ApplicationScoped;
@@ -19,12 +20,25 @@ public class DamageTypeConverter implements Converter {
 
     @Override
     public Object getAsObject(FacesContext context, UIComponent component, String value) {
-       
+       if (value == null){
+           return "";
+       }
+       DamageType damType = dtServ.getDamageTypeById(Long.parseLong(value));
+       if(damType == null){
+           return "";
+       }
+       return damType;
     }
 
     @Override
     public String getAsString(FacesContext context, UIComponent component, Object value) {
-        
+        if(value == null){
+            return null;
+        }
+        if(!value.getClass().equals(DamageType.class)){
+            return null;
+        }
+        return String.valueOf(((DamageType)value).getDamageTypeID());
     }
     
 }
