ad60d17e2af9ef4ceabac9781b357af53a4e1685
Frank <test@test.com>
2018-01-17 10:07:13 +0100

    try catch

--- src/main/java/de/othr/insurance/service/BankService.java
+++ src/main/java/de/othr/insurance/service/BankService.java
@@ -22,16 +22,24 @@ public class BankService {
         
     @Transactional
     public boolean doTransfer(String transmitter, String receiver, double amount, String purpose){
+        try {
         port = service.getTransferServicePort();
         result = port.createTransfer(transmitter, receiver, amount, purpose);
+        } catch(Exception ex) {
+            throw new RuntimeException("Error: Could not create Transfer "+ex.getMessage(), ex);
+        }
         return result != null;
     }
     
     @Transactional
     public boolean checkIban(String iban){
+        try {
         port = service.getTransferServicePort();
         System.out.println("iban: "+iban);
         customer = port.checkIban(iban);
+        } catch( Exception ex) {
+            throw new RuntimeException("Error: Could not check Iban "+ex.getMessage(), ex);
+        }
         return customer != null;
     }
 }
